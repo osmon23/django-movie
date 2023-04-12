743b904cf7cecb12a8da0ac4b427aba48f75ccbd
Osmon23 <oma_23_03_98@mail.ru>
2023-04-12 15:32:51 +0600

    videos 7-8, 9-10

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.MoviesView.as_view())
+    path('', views.MoviesView.as_view()),
+    path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
+    path('review/<int:pk>/', views.AddReview.as_view(), name='add_review'),
 ]
